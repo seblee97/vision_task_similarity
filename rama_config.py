07b1379b61875701edd080578dccefe47a4ad090
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-10-13 16:31:37 +0100

    initial commit

---
+++ rama_config.py
@@ -0,0 +1,23 @@
+from typing import Dict, List, Union
+
+import rama_config_template
+from config_manager import base_configuration
+
+
+class RamaConfig(base_configuration.BaseConfiguration):
+    def __init__(self, config: Union[str, Dict], changes: List[Dict] = []) -> None:
+        super().__init__(
+            configuration=config,
+            template=rama_config_template.RamaConfigTemplate.base_template,
+            changes=changes,
+        )
+
+        self._validate_config()
+
+    def _validate_config(self) -> None:
+        """Check for non-trivial associations in config.
+
+        Raises:
+            AssertionError: if any rules are broken by config.
+        """
+        pass
