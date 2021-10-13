7765d35cd53f97d8b3036bc145d92641998124d7
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-10-13 16:30:50 +0100

    initial commit

---
+++ rama_config_template.py
@@ -0,0 +1,80 @@
+import constants
+from config_manager import config_field, config_template
+
+
+class RamaConfigTemplate:
+
+    _tasks_template = config_template.Template(
+        fields=[
+            config_field.Field(
+                name=constants.INDICES,
+                types=[list],
+                requirements=[
+                    lambda x: all([isinstance(i, list) for i in x]),
+                    lambda x: all([all([isinstance(j, int) for j in i]) for i in x]),
+                    lambda x: all([all([j >= 0 and j < 10 for j in i]) for i in x]),
+                ],
+            ),
+            config_field.Field(
+                name=constants.MIXING,
+                types=[list],
+                requirements=[lambda x: all([j >= 0 and j <= 1 for j in x])],
+            ),
+        ],
+        # nested_templates=[_indices_template],
+        level=[constants.TASKS],
+    )
+
+    _training_template = config_template.Template(
+        fields=[
+            config_field.Field(
+                name=constants.TOTAL_EPOCHS, types=[int], requirements=[lambda x: x > 0]
+            ),
+            config_field.Field(
+                name=constants.SWITCH_EPOCH, types=[int], requirements=[lambda x: x > 0]
+            ),
+            config_field.Field(
+                name=constants.LEARNING_RATE,
+                types=[int, float],
+                requirements=[lambda x: x > 0],
+            ),
+            config_field.Field(
+                name=constants.BATCH_SIZE, types=[int], requirements=[lambda x: x > 0]
+            ),
+        ],
+        level=[constants.TRAINING],
+    )
+
+    _network_template = config_template.Template(
+        fields=[
+            config_field.Field(
+                name=constants.INPUT_DIMENSION,
+                types=[int],
+                requirements=[lambda x: x > 0],
+            ),
+            config_field.Field(
+                name=constants.HIDDEN_DIMENSION,
+                types=[int],
+                requirements=[lambda x: x > 0],
+            ),
+            config_field.Field(
+                name=constants.OUTPUT_DIMENSION,
+                types=[int],
+                requirements=[lambda x: x > 0],
+            ),
+            config_field.Field(
+                name=constants.NONLINEARITY,
+                types=[str],
+                requirements=[lambda x: x in [constants.RELU, constants.SIGMOID]],
+            ),
+        ],
+        level=[constants.NETWORK],
+    )
+
+    base_template = config_template.Template(
+        fields=[
+            config_field.Field(name=constants.SEED, types=[int]),
+            config_field.Field(name=constants.USE_GPU, types=[bool]),
+        ],
+        nested_templates=[_tasks_template, _training_template, _network_template],
+    )
