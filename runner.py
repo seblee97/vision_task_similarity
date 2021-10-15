3c4df021f4ad4bc0701fca1a69783cb0a12aa38e
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-10-15 23:51:16 +0100

    tensor to float before logging

--- runner.py
+++ runner.py
@@ -271,6 +271,6 @@ class Runner(base_runner.BaseRunner):
             derivative = torch.autograd.grad(loss, post_activation)[0]
 
             for node_index, node_derivative in enumerate(derivative[0]):
-                node_fischers[node_index] += node_derivative.detach() ** 2 / size
+                node_fischers[node_index] += node_derivative.detach().item() ** 2 / size
 
         return node_fischers
