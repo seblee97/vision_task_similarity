5c60c30f68eeff1bc1ccbab1f4da3861865cf4e5
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-11-08 14:24:10 +0000

    initial commit of submitit style script

---
+++ rama/submit.py
@@ -0,0 +1,37 @@
+import submitit
+from rama import rama_config, runner
+from run_modes import single_run
+
+runner_class = runner.Runner
+config_class = rama_config.RamaConfig
+
+single_run.single_run(
+    runner_class=runner_class,
+    config_class=config_class,
+    config_path="config.yaml",
+    checkpoint_path="log_exp",
+    run_methods=["train", "post_process"],
+)
+
+executor = submitit.AutoExecutor(folder="log_exp")
+
+executor.update_parameters(
+    timeout_min=20,
+    mem_gb=30,
+    gpus_per_node=1,
+    cpus_per_task=8,
+    slurm_array_parallelism=256,
+    slurm_partition="gpu",
+)
+
+jobs = []
+with executor.batch():
+    for seed in range(1):
+        job = executor.submit(
+            single_run.single_run,
+            runner_class=runner_class,
+            config_class=config_class,
+            config_path="config.yaml",
+            checkpoint_path="log_exp",
+            run_methods=["train", "post_process"],
+        )
