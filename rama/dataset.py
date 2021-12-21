2d080077cb3dfe4b356513d8add1d961519a8cad
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-12-21 18:36:04 +0100

    remove normalisation for now

--- rama/dataset.py
+++ rama/dataset.py
@@ -52,7 +52,7 @@ class FashionMNIST:
         [
             torchvision.transforms.Resize(32),
             torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize((0.5,), (0.5)),
+            # torchvision.transforms.Normalize((0.5,), (0.5)),
         ]
     )
 
